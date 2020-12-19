36d20479ef5c997f56b43ea54541ef5aad5522fd
pixel-mine <74670420+pixel-mine@users.noreply.github.com>
2020-12-19 16:10:17 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-main","displayName":"Main","link":{"linkType":"LinkTypePage","href":"#!page-main"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
